97c72979f839baf026993dae02be56389b0e6d99
Noam Jehoshua <Noam10012@gmail.com>
2020-10-17 21:34:48 +0300

    basic flask works, not very well. deleted badly tracked folders.

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 HTML ="""<!DOCTYPE HTML>
      <html>
-        <head></head>
+        <head><title>Movie by search</title></head>
         <body>
             <h1>A page title.</h1>
-            <div><img src="@@@poster@@@" alt="movie poster"></div>
+            <div>@@@overview@@@ </div>
+            <div><img src="@@@poster_img@@@" alt="movie poster"></div>
         </body>
     </html>
 
@@ -16,8 +17,15 @@ HTML ="""<!DOCTYPE HTML>
 @app.route('/<movie_q>')
 def hello_world(movie_q):
     movie_q=movie_q.lower()
-    response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key=b1a92c686f2982862df95e0cdf1b9c38&language=en-US&query=2067&page=1&include_adult=false')
-    resp_json = response.json()
-    #poster = resp_json['results'][0]['poster_path']
-    #return HTML.replace("@@@poster@@@", poster)
-    return HTML
+    resp = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key=b1a92c686f2982862df95e0cdf1b9c38&language=en-US&query={movie_q}&page=1&include_adult=false')
+    resp_json = resp.json()
+    poster = resp_json['results'][0]["poster_path"]
+    overview = resp_json['results'][0]["overview"]
+    #movie_id = resp_json['results'][0]["id"]
+    #print(poster)
+    poster2 = requests.get(f'http://image.tmdb.org/t/p/w500/{poster}')
+
+    result = HTML.replace("@@@poster_img@@@", poster)
+    result =HTML.replace("@@@overview@@@", overview)
+    return result
+    
