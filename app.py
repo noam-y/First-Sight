cb343b841900da41ee9ac89bf39b335a4636feca
Noam Jehoshua <Noam10012@gmail.com>
2020-10-18 12:38:07 +0300

    added basic template of movie list (not used yet), flask app now shows all movie results- not just the first. shows poster & overview.

--- app.py
+++ app.py
@@ -2,21 +2,48 @@ from flask import Flask,render_template
 import requests
 app = Flask(__name__)
 
+html =""" <!DOCTYPE HTML>
+     <html>
+        <head><title>Movie by search</title></head>
+        <body >
+            <h1>movie list</h1>
+
+            <div style="display:grid; grid-template-columns: 1fr 1fr 1fr;">@@@mvlist@@@</div>
+
+        </body>
+    </html>
+"""
 
 
 @app.route('/<movie_q>')
-def hello_world(movie_q):
+def movie_search(movie_q):
     movie_q=movie_q.lower()
     resp = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key=b1a92c686f2982862df95e0cdf1b9c38&language=en-US&query={movie_q}&page=1&include_adult=false')
     resp_json = resp.json()
-    poster = resp_json['results'][0]["poster_path"]
-    overview = resp_json['results'][0]["overview"]
-    #movie_id = resp_json['results'][0]["id"]
-    #print(poster)
-    posterquery = 'http://image.tmdb.org/t/p/w500'+poster
+    #movie_title = resp_json['results'][0]["original_title"]
 
+    all_movie_details = ''
+    for movie in resp_json['results']:
+        movie_details = render_template(
+                'one-movie.html',
+                movie_poster = 'http://image.tmdb.org/t/p/w500'+movie["poster_path"],
+                movie_overview = movie["overview"],
+            
+        )
+
+        print(movie_details)
+
+        all_movie_details = all_movie_details + movie_details
+
+    #all_movie_details = """{% extends "./templates/movie-list.html" %}
+    #{% block themovies %}""" + all_movie_details + """ {% endblock %}"""
+    toprint = html.replace("@@@mvlist@@@", all_movie_details)
+    return toprint
     return render_template(
-        'one-movie.html',
-        overview=overview,
-        poster_query=posterquery,
+        'movie-list.html',
+        mvlist = all_movie_details,
     )
+
+
+
+
