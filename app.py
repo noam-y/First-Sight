9ac10fad40cbe63656d1901dc851dfbca4f80a2d
Noam Jehoshua <Noam10012@gmail.com>
2020-10-23 13:04:58 +0300

    formatting stuff

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def homepage():
         text_search = request.form['searchtext']
         return redirect(url_for("movie_search", movie_q=text_search))
     else:
-        return render_template('index.html', genres=get_movie_genres())
+        return render_template('index.j2', genres=get_movie_genres())
 
 
 @app.route('/suggested-list')
@@ -54,14 +54,14 @@ def category_search():
     if request.method == 'POST':
         genres = ','.join(request.form.getlist('genres'))
         include_adult = request.form.get('agelimit') is not None
-        time_limit = request.form.get('length') 
+        time_limit = request.form.get('length')
         search_query = f'https://api.themoviedb.org/3/discover/movie?api_key={API_KEY}&include_adult={include_adult}&with_genres={genres}&page=1'
 
         # checks that time limit input is valid
         if time_limit != '':
             search_query = search_query + f'&with_runtime.lte={time_limit}'
 
-        return sort_out_movies(search_query)  
+        return sort_out_movies(search_query)
 
     else:
         return '<p>Try again.!!!</p>'
@@ -84,43 +84,44 @@ def get_by_id(movie_id):
 
 
 def sort_out_movies(search_query):
-        resp = requests.get(search_query)
-        resp_json = resp.json()
+    resp = requests.get(search_query)
+    resp_json = resp.json()
 
-        json_movie_list = []
-        for movie in resp_json['results']:
-            json_movie_list.append(sort_one_movie(movie))
+    json_movie_list = []
+    for movie in resp_json['results']:
+        json_movie_list.append(sort_one_movie(movie))
 
-        return render_template( 
-            'recommended-movies.html',
-            mvlist=json_movie_list,
-        )
+    return render_template(
+        'recommended-movies.j2',
+        mvlist=json_movie_list,
+    )
 
 
 def sort_one_movie(movie, single=False):
     if 'title' in movie:
         try:
-            json_movie_details = {'title': movie["title"], 'overview': movie['overview'], 'poster': 'http://image.tmdb.org/t/p/w500' + movie["poster_path"], 'id': movie["id"]}
+            json_movie_details = {'title': movie["title"], 'overview': movie['overview'],
+                                  'poster': 'http://image.tmdb.org/t/p/w500' + movie["poster_path"], 'id': movie["id"]}
         except(TypeError):
             json_movie_details = {
                 'title': movie["title"], 'overview': movie['overview'], 'poster': QUESTION_MARK, 'id': movie["id"]}
 
     else:
         try:
-            json_movie_details = {'title': movie["name"], 'overview': movie['overview'], 'poster': 'http://image.tmdb.org/t/p/w500' + movie["poster_path"], 'id': movie["id"]}
+            json_movie_details = {'title': movie["name"], 'overview': movie['overview'],
+                                  'poster': 'http://image.tmdb.org/t/p/w500' + movie["poster_path"], 'id': movie["id"]}
         except(TypeError):
-            json_movie_details = {'title': movie["name"], 'overview': movie['overview'], 'poster': QUESTION_MARK, 'id': movie["id"]}
+            json_movie_details = {
+                'title': movie["name"], 'overview': movie['overview'], 'poster': QUESTION_MARK, 'id': movie["id"]}
 
     if single:
         return render_template(
-            'onemovie.html',
+            'onemovie.j2',
             mv=json_movie_details,
         )
     else:
         return json_movie_details
-            
+
 
 if __name__ == "__main__":
     app.run()
-
-
